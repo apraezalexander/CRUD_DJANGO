10d0ac792f22b32805ecde51923df3e682791e62
alexander apraez <apraez.consultant@gmail.com>
2024-06-05 17:18:09 -0500

    configuración de la página de inicio

--- cafenar/productos/views.py
+++ cafenar/productos/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def holaMundo(request):
+    return HttpResponse("Bienvenidos a la tienda virtual")
+
+def inicio(request):
+    return render(request, 'inicio.html')
